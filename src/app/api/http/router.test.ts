2ad1c051f2c2415b72bd2d742ce261a62d1bf853
LeoFC97 <leonardofernancesc@gmail.com>
2021-07-04 00:29:07 -0300

    tests: add more tests to routers

--- src/app/api/http/router.test.ts
+++ src/app/api/http/router.test.ts
@@ -6,4 +6,21 @@ describe('Check if all endpoints are declarated', () => {
     const healthEnpoint = router.stack.find((endpoint) => endpoint.route.path === pathToBeTested);
     expect(healthEnpoint).toBeDefined();
   });
+  test('Should exists method GET on /health', async () => {
+    const pathToBeTested = '/health';
+    const healthMethod = router.stack.some((endpoint) => endpoint.route.path === pathToBeTested
+      && endpoint.route.methods.get);
+    expect(healthMethod).toBeTruthy();
+  });
+  test('Should exist path /store', async () => {
+    const pathToBeTested = '/store';
+    const healthEnpoint = router.stack.find((endpoint) => endpoint.route.path === pathToBeTested);
+    expect(healthEnpoint).toBeDefined();
+  });
+  test('Should exists method POST on /store', async () => {
+    const pathToBeTested = '/store';
+    const healthMethod = router.stack.some((endpoint) => endpoint.route.path === pathToBeTested
+      && endpoint.route.methods.post);
+    expect(healthMethod).toBeTruthy();
+  });
 });
