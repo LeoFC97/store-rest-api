d699789a4090032483ff1c8779ca1faad9d37c8f
LeoFC97 <leonardofernancesc@gmail.com>
2021-07-05 02:00:35 -0300

    feat: add handle domain error middleware

---
+++ src/app/api/http/middlewares/handle-domain-errors.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+import InternalServerHttpError from '../errors/internal';
+import ValidationError from '../../../errors/validation';
+import BadRequestHttpError from '../errors/bad-request';
+import ResourceNotFoundError from '../../../errors/resource-not-found';
+import NotFoundHttpError from '../errors/not-found';
+import EntityNotFoundError from '../../../errors/entity-not-found';
+
+export default (error: Error, req: Request, _res: Response, next: NextFunction): void => {
+  if (error instanceof ValidationError) return next(new BadRequestHttpError(error));
+
+  if (error instanceof ResourceNotFoundError) return next(new NotFoundHttpError(error));
+
+  if (error instanceof EntityNotFoundError) return next(new NotFoundHttpError(error));
+
+  console.error(error);
+  return next(new InternalServerHttpError(error));
+};
