92a0f9eb9ab26091a2182bc2a10b036e6fee06f8
LeoFC97 <leonardofernancesc@gmail.com>
2021-07-05 01:57:27 -0300

    feat: add store endpoint

--- src/app/api/http/router.ts
+++ src/app/api/http/router.ts
@@ -3,9 +3,11 @@ import container from '../../container';
 import { adapt } from '../../drivers/http/express-router-adapter';
 
 import HealthController from './controllers/health';
+import CreateStoreController from './controllers/store/create-store';
 
 const router = express.Router();
 
 router.get('/health', adapt(container.resolve(HealthController)));
+router.post('/store', adapt(container.resolve(CreateStoreController)));
 
 export default router;
