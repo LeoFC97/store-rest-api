ac1c42007693c774dff6cbc158ff0b8595cc6776
LeoFC97 <leonardofernancesc@gmail.com>
2021-07-06 01:35:02 -0300

    feat: add store mock

--- src/test/mocks/store.ts
+++ src/test/mocks/store.ts
@@ -1,10 +1,32 @@
+import { ObjectId } from 'mongodb';
+import { CreateStoreBodyData, CreateStoreUseCaseData } from 'app/interfaces/entities/store/store';
 import mockUser from './user';
 import { validAdress } from './address';
 
-// eslint-disable-next-line import/prefer-default-export
 export const createStoreData = {
   name: 'Store name for testing',
   pictureUrL: 'www.urlForMocking.com',
   createdBy: mockUser,
   address: validAdress,
+};
+
+export const createStoreBodyDataMock: CreateStoreBodyData = {
+  address: validAdress,
+  name: 'Store name for testing',
+  pictureUrL: 'www.urlForMocking.com',
+};
+
+export const createStoreUseCaseDataMock: CreateStoreUseCaseData = {
+  store: createStoreBodyDataMock,
+  user: mockUser,
+};
+
+export default {
+  _id: new ObjectId('60c3ca93aea1c953388aa07b'),
+  name: 'Store name for testing',
+  pictureUrL: 'www.urlForMocking.com',
+  address: validAdress,
+  createdBy: mockUser,
+  createdAt: new Date(),
+  updatedAt: new Date(),
 };
