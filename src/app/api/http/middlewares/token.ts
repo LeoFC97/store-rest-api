409593566c9a41a4716f93a268d2d05d6efb30da
LeoFC97 <leonardofernancesc@gmail.com>
2021-07-06 01:33:32 -0300

    feat: add token manager interfaces, tests and implementation

---
+++ src/app/api/http/middlewares/token.ts
@@ -0,0 +1,22 @@
+import { Response, NextFunction } from 'express';
+import { inject, injectable } from 'tsyringe';
+import ExpressMiddleware, { RequestExpress } from '../../../interfaces/http/express';
+import TokenManager from '../../../interfaces/http/token-manager';
+
+@injectable()
+class DecodeTokenMiddleware implements ExpressMiddleware {
+  constructor(
+    @inject('TokenManager') public jwtTokenManager: TokenManager,
+  ) {}
+  execute(req: RequestExpress, res: Response, next: NextFunction): void {
+    if (req.headers?.authorization) {
+      const decodedToken = this.jwtTokenManager.decode(req.headers.authorization);
+      req.user = {
+        email: decodedToken.user.email,
+        name: decodedToken.user.fullName,
+      };
+    }
+    next();
+  }
+}
+export default DecodeTokenMiddleware;
