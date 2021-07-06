6beefab3642515ce93d1dc637e02265c08ec89bc
LeoFC97 <leonardofernancesc@gmail.com>
2021-07-06 01:33:56 -0300

    feat: add middlewares at express app

--- src/app/drivers/http/server.js
+++ src/app/drivers/http/server.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import router from '../../api/http/router';
+import container from '../../container';
+import { adapt } from './express-middleware-adapter';
+import DecodeTokenMiddleware from '../../api/http/middlewares/token';
+import PathNotFoundMiddleware from '../../api/http/middlewares/path-not-found';
+import handleDomainErrors from '../../api/http/middlewares/handle-domain-errors';
 
 const startExpressServer = () => {
   const app = express();
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-
+  app.use(adapt(container.resolve(DecodeTokenMiddleware)));
   app.use('/', router);
+  app.use(adapt(container.resolve(PathNotFoundMiddleware)));
+  app.use(handleDomainErrors);
 
   return app;
 };
