165b964374cf54885a15133727738c05392b7157
LeoFC97 <leonardofernancesc@gmail.com>
2021-07-06 01:34:24 -0300

    feat: add create store use case and test

---
+++ src/app/use-cases/store/create-store.test.ts
@@ -0,0 +1,42 @@
+import StoreMongoDBRepository from '../../drivers/mongodb/store/store-mongodb-repository';
+import container from '../../container';
+import Store from '../../interfaces/entities/store/store';
+import StoreMock, { createStoreUseCaseDataMock } from '../../../test/mocks/store';
+
+import CreateStoreUseCase from './create-store';
+
+let createStoreMongoDBRepositorySpy:jest.SpyInstance<Promise<Store>>;
+
+const storeMongoDBRepositoryResolved = container.resolve(StoreMongoDBRepository);
+
+const makeSpyOnExecuteUseCase = () => {
+  createStoreMongoDBRepositorySpy = jest.spyOn(storeMongoDBRepositoryResolved, 'create');
+  createStoreMongoDBRepositorySpy.mockImplementation(
+    async () => new Promise((resolve) => resolve(StoreMock)),
+  );
+};
+
+describe('Create store use case', () => {
+  beforeEach(async () => {
+    makeSpyOnExecuteUseCase();
+  });
+  test('Should call create store repository with correct adjustment id', async () => {
+    const useCase = new CreateStoreUseCase(storeMongoDBRepositoryResolved);
+    const expectedCreateStoreData = {
+      address: {
+        cep: '00000000',
+        complement: 'complement for mocking',
+        number: 123,
+        street: 'Number',
+      },
+      createdBy: {
+        email: 'mock@mockMail.com',
+        name: 'Mock Name',
+      },
+      name: 'Store name for testing',
+      pictureUrL: 'www.urlForMocking.com',
+    };
+    await useCase.execute(createStoreUseCaseDataMock);
+    expect(createStoreMongoDBRepositorySpy).toHaveBeenCalledWith(expectedCreateStoreData);
+  });
+});
