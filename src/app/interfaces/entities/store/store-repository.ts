23e2e3c63942d494a38fce88d68baa8e7ab8a99c
Leonardo Costa <leonardo.fc2@outlook.com>
2021-07-08 01:14:18 -0300

    fix: type of param

--- src/app/interfaces/entities/store/store-repository.ts
+++ src/app/interfaces/entities/store/store-repository.ts
@@ -1,7 +1,6 @@
-import { ObjectId } from 'mongoose';
 import Store, { CreateStoreData } from './store';
 
 export default interface StoreRepository {
   create(data: CreateStoreData): Promise<Store>;
-  deleteById(storeString: ObjectId): Promise<boolean>;
+  deleteById(storeString: string): Promise<boolean>;
 }
