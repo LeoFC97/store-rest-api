ba4a47063d87dc7c94bdcc79430da76198ea3a83
LeoFC97 <leonardo.fc2@outlook.com>
2021-07-09 01:45:35 -0300

    feat: add array of stores mock

--- src/test/mocks/store.ts
+++ src/test/mocks/store.ts
@@ -1,7 +1,28 @@
 import { ObjectId } from 'mongodb';
-import { CreateStoreBodyData, CreateStoreUseCaseData } from 'app/interfaces/entities/store/store';
+import Store, { CreateStoreBodyData, CreateStoreUseCaseData } from 'app/interfaces/entities/store/store';
 import mockUser from './user';
 import { validAdress } from './address';
+
+export const arrayOfStores :Store[] = [
+  {
+    _id: new ObjectId('60c3ca93aea1c953388aa07b'),
+    name: 'Store 2',
+    pictureUrL: 'www.urlForMocking.com',
+    address: validAdress,
+    createdBy: mockUser,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    _id: new ObjectId('60c3ca13ae21c933388aa07b'),
+    name: 'Store 1',
+    pictureUrL: 'www.urlForMocking.com',
+    address: validAdress,
+    createdBy: mockUser,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
 
 export const createStoreData = {
   name: 'Store name for testing',
