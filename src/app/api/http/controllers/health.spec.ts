fcaccaaf1ba5dc36c1deec70a1e0af6e62582931
LeoFC97 <leonardo.fc2@outlook.com>
2021-07-10 17:29:45 -0300

    refactor: refactor integration test

--- src/app/api/http/controllers/health.spec.ts
+++ src/app/api/http/controllers/health.spec.ts
@@ -1,23 +1,24 @@
 import supertest from 'supertest';
 import startExpressServer from '../../../drivers/http/server';
-import SetupDatabaseForTests from '../../../../test/setup-database-for-tests';
+import MongoDbManager from '../../../drivers/mongodb/mongodb-manager';
 
 describe('Test health endpoints', () => {
   afterEach(async () => {
     jest.clearAllMocks();
-    const connection = await SetupDatabaseForTests.getConnection();
-    await SetupDatabaseForTests.closeConnection(connection);
   });
   test('Should return 200 when database is connected', async () => {
     const apiMocked = await supertest(startExpressServer());
-    await SetupDatabaseForTests.initializeMongoInMemory();
+    const mongoDbManager: jest.SpyInstance<boolean> = jest.spyOn(MongoDbManager, 'isConnected');
+    mongoDbManager.mockImplementation(
+      () => true,
+    );
     const result = await apiMocked.get('/health');
     expect(result.statusCode).toBe(200);
   });
   test('Should return 500 when database is not connected', async () => {
-    const setupDatabaaseForTests: jest.SpyInstance<Promise<void>> = jest.spyOn(SetupDatabaseForTests, 'initializeMongoInMemory');
-    setupDatabaaseForTests.mockImplementation(
-      async () => new Promise((resolve, reject) => (new Error())),
+    const mongoDbManager: jest.SpyInstance<boolean> = jest.spyOn(MongoDbManager, 'isConnected');
+    mongoDbManager.mockImplementation(
+      () => false,
     );
     const apiMocked = await supertest(startExpressServer());
     const result = await apiMocked.get('/health');
