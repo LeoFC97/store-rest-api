17ed37fbd428fddbd54b2c2c6eb5af75261add0f
LeoFC97 <leonardo.fc2@outlook.com>
2021-07-11 04:50:16 -0300

    feat: throw error if not found store

--- src/app/use-cases/store/find-store-by-id.ts
+++ src/app/use-cases/store/find-store-by-id.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 import Store from '../../interfaces/entities/store/store';
 import UseCase from '../../interfaces/use-case';
 import StoreRepository from '../../interfaces/entities/store/store-repository';
+import EntityNotFoundError from 'app/errors/entity-not-found';
 
 @injectable()
 class FindStoreByIdUseCase implements UseCase {
@@ -11,7 +12,11 @@ class FindStoreByIdUseCase implements UseCase {
 
   async execute(storeIdToBeRemoved: string): Promise<Store> {
     const store = await this.storeRepository.getById(storeIdToBeRemoved);
-    return store;
+    if (!store) {
+      throw new EntityNotFoundError('store not found for this id');
+    } else {
+      return store;
+    }
   }
 }
 export default FindStoreByIdUseCase;
