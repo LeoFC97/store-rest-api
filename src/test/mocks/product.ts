de94979d36192fa0ed49d4d4e938077e2659f32c
LeoFC97 <leonardo.fc2@outlook.com>
2021-07-11 16:06:51 -0300

    chore: add product mocks

---
+++ src/test/mocks/product.ts
@@ -0,0 +1,34 @@
+import { ObjectId } from 'mongodb';
+import { CreateProductData, CreateProductBodyData, CreateProductUseCaseData } from 'app/interfaces/entities/product/product';
+import mockUser from './user';
+
+export const createProductData: CreateProductData = {
+  name: 'Mocked Store',
+  pictureUrL: 'www.urlForMocking.com',
+  price: 3990,
+  category: 'PIZZA',
+  createdBy: mockUser,
+};
+
+export const createProductBodyDataMock: CreateProductBodyData = {
+  name: 'Store name for testing',
+  pictureUrL: 'www.urlForMocking.com',
+  category: 'TACOS',
+  price: 1200,
+};
+
+export const createStoreUseCaseDataMock: CreateProductUseCaseData = {
+  product: createProductBodyDataMock,
+  user: mockUser,
+};
+
+export default {
+  _id: new ObjectId('60a3ca93a2a1c953388aa07b'),
+  name: 'Pizza mocked for testing',
+  price: 7990,
+  category: 'PIZZA',
+  pictureUrL: 'www.urlForMocking.com',
+  createdBy: mockUser,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
