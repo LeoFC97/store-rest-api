729f6984a4e03a9daef6e769f0ea258cf774c6a0
LeoFC97 <leonardo.fc2@outlook.com>
2021-07-11 21:15:32 -0300

    feat: add new controller to router and test

--- src/app/api/http/router.ts
+++ src/app/api/http/router.ts
@@ -3,10 +3,13 @@ import container from '../../container';
 import { adapt } from '../../drivers/http/express-router-adapter';
 
 import HealthController from './controllers/health';
+
 import CreateStoreController from './controllers/store/create-store';
 import ListAllStoresController from './controllers/store/list-all-stores';
 import DeleteStoreByIdController from './controllers/store/delete-store-by-id';
 import FindStoreByIdController from './controllers/store/find-store';
+
+import CreateProductController from './controllers/product/create-product';
 
 const router = express.Router();
 
@@ -15,5 +18,7 @@ router.get('/stores', adapt(container.resolve(ListAllStoresController)));
 router.post('/store', adapt(container.resolve(CreateStoreController)));
 router.delete('/store/:storeId', adapt(container.resolve(DeleteStoreByIdController)));
 router.get('/store/:storeId', adapt(container.resolve(FindStoreByIdController)));
+
+router.post('/product', adapt(container.resolve(CreateProductController)));
 
 export default router;
